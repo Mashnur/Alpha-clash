9ad3961c30d8153b03cd80c3de5a13cd5318434c
Mashnur <221007712@eastdelta.edu.bd>
2024-02-19 02:27:47 +0600

    added the backgtound color of the keybord basis on selecting the alphabet

--- Script/alpha.js
+++ Script/alpha.js
@@ -1,4 +1,17 @@
+function continuegame(){
+    const alphabet= getRandomAlphabet()
+    console.log("your random alphabet",alphabet);
+    const currentalphabet= document.getElementById('current-alphabet')
+    currentalphabet.innerText=alphabet;
+    addbackgroundcolour(alphabet)
+
+}
+
+
+
+
 function play(){
     HideElementByid('home-screen')
     ShowElementById('play-screen');
+    continuegame()
 }
--- Script/utility.js
+++ Script/utility.js
@@ -6,3 +6,16 @@ function ShowElementById(elementId){
     const element =document.getElementById(elementId);
     element.classList.remove('hidden')
 }
+function getRandomAlphabet(){
+    const alphabetstring="abcdefghijklmnopqrstuvwxyz";
+    const alphabets= alphabetstring.split('');
+    // console.log(alphabet)
+    const RandomNumber= Math.random()*25;
+    const index= Math.round(RandomNumber);
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+function addbackgroundcolour(elementId){
+    const elementcolor= document.getElementById(elementId);
+    elementcolor.classList.add('bg-orange-400');
+}
